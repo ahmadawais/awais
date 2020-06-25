af3ae702f0f666e1377a50f190f94b89091b7d8d
Ahmad Awais <me@AhmadAwais.com>
2020-06-25 14:26:13 +0500

    📦 NEW: Welcome header

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-const clearConsole = require('clear-any-console');
-
-// Clears the console.
-clearConsole();
+welcome({
+	title: pkgJSON.name,
+	tagLine: `Howdy, nice to meet ya!`,
+	description: pkgJSON.description,
+	version: pkgJSON.version,
+	bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: false,
+});
 
 console.log(`
 Ahmad Awais
