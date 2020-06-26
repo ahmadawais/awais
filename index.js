fada8597039cb883a462e8fba22c4baacf73ba0c
Ahmad Awais <me@AhmadAwais.com>
2020-06-26 15:29:53 +0500

    📦 NEW: Colors

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 #!/usr/bin/env node
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
+const log = console.log;
+const dim = chalk.dim;
+const italic = chalk.italic;
+const twitterClr = chalk.hex(`#1da1f2`).bold.inverse;
+const githubClr = chalk.hex(`#6cc644`).bold.inverse;
+const purple = chalk.hex(`#6937FF`).bold.inverse;
 
 welcome({
-	title: pkgJSON.name,
+	title: `Ahmad Awais`,
 	tagLine: `Howdy, nice to meet ya!`,
 	description: pkgJSON.description,
 	version: pkgJSON.version,
-	bgColor: `#FADC00`,
+	bgColor: `#6937FF`,
 	color: `#000000`,
 	bold: true,
 	clear: true,
 });
 
-console.log(`
-Ahmad Awais
+log(`${italic(
+	`Award-winning open-source engineer and developer advocate. Author of various open-source dev-tools and software libraries used by millions of developers worldwide. Leading developers and publishing technical content for over a decade. Self-confessed tech comedian cracking silly jokes in the JavaScript web and cloud communities.`
+)}
 
-Award-winning open-source engineer and developer advocate. Author of various open-source dev-tools and software libraries used by millions of developers worldwide. Leading developers and publishing technical content for over a decade. Self-confessed tech comedian cracking silly jokes in the JavaScript web and cloud communities.
-
-🐦 Twitter: https://twitter.com/MrAhmadAwais
-📖 GitHub:  https://github.com/AhmadAwais
-😎 Blog:    https://AhmadAwais.com
+${twitterClr(` Twitter `)} ${dim(`https://twitter.com/MrAhmadAwais`)}
+${githubClr(` GitHub `)}  ${dim(`https://github.com/AhmadAwais`)}
+${purple(` Blog `)}    ${dim(`https://AhmadAwais.com`)}
 `);
