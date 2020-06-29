822ed243d54671dc921993081c407dac9d7bdbeb
Ahmad Awais <me@AhmadAwais.com>
2020-06-29 10:59:26 +0500

    📦 NEW: Check Node version

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
 const chalk = require('chalk');
 const alert = require('cli-alerts');
+const checkNode = require('cli-check-node');
+
 const dim = chalk.dim;
 const italic = chalk.italic;
 const twitterClr = chalk.hex(`#1da1f2`).bold.inverse;
@@ -19,6 +21,8 @@ welcome({
 	bold: true,
 	clear: true,
 });
+
+checkNode('10');
 
 console.log(`${italic(
 	`Award-winning open-source engineer and developer advocate. Author of various open-source dev-tools and software libraries used by millions of developers worldwide. Leading developers and publishing technical content for over a decade. Self-confessed tech comedian cracking silly jokes in the JavaScript web and cloud communities.`
