8d46378c4b98db4af0e991f16f912edc2fbdb063
Ahmad Awais <me@AhmadAwais.com>
2020-06-30 08:02:38 +0500

    🚀 RELEASE: 2.7.0

--- index.js
+++ index.js
@@ -34,9 +34,7 @@ const ad = printAd ? adInfo : ``;
 			`
 Award-winning open-source engineer and developer advocate. Author of various open-source dev-tools and software libraries used by millions of developers worldwide. Leading developers and publishing technical content for over a decade. Self-confessed tech comedian cracking silly jokes in the JavaScript web and cloud communities.`
 		)}
-
 ${social}
-
 ${ad}
 `
 	);
