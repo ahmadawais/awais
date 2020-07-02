cbbe3851d940996b14457dcbd9e9430e0a6e9b28
Ahmad Awais <me@AhmadAwais.com>
2020-07-02 08:36:35 +0500

    📦 NEW: Meow

--- utils/data.js
+++ utils/data.js
@@ -7,9 +7,9 @@ const githubClr = chalk.hex(`#6cc644`).bold.inverse;
 const purple = chalk.hex(`#6937FF`).bold.inverse;
 
 const bio = `${italic(
-	`
-Award-winning open-source engineer and developer advocate. Author of various open-source dev-tools and software libraries used by millions of developers worldwide. Leading developers and publishing technical content for over a decade. Self-confessed tech comedian cracking silly jokes in the JavaScript web and cloud communities.`
-)}`;
+	`Award-winning open-source engineer and developer advocate. Author of various open-source dev-tools and software libraries used by millions of developers worldwide. Leading developers and publishing technical content for over a decade. Self-confessed tech comedian cracking silly jokes in the JavaScript web and cloud communities.`
+)}
+`;
 
 const social = `
 ${twitterClr(` Twitter `)} ${dim(`https://twitter.com/MrAhmadAwais`)}
--- utils/init.js
+++ utils/init.js
@@ -4,19 +4,22 @@ const unhandled = require('cli-handle-unhandled');
 
 const pkgJSON = require('./../package.json');
 
-module.exports = () => {
+module.exports = (minimal, clear) => {
 	unhandled();
 
-	welcome({
-		title: `Ahmad Awais`,
-		tagLine: `Howdy, nice to meet ya!`,
-		description: pkgJSON.description,
-		version: pkgJSON.version,
-		bgColor: `#6937FF`,
-		color: `#000000`,
-		bold: true,
-		clear: true,
-	});
+	!minimal &&
+		welcome({
+			title: `Ahmad Awais`,
+			tagLine: `Howdy, nice to meet ya!`,
+			description: pkgJSON.description,
+			version: pkgJSON.version,
+			bgColor: `#6937FF`,
+			color: `#000000`,
+			bold: true,
+			clear,
+		});
+
+	minimal && console.log(`Ahmad Awais`);
 
 	checkNode('10');
 };
