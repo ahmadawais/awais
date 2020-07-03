23f174d71bcd19bd61203f32042cc7519f683356
Ahmad Awais <me@AhmadAwais.com>
2020-07-03 10:29:41 +0500

    👌 IMPROVE: Spinners

--- utils/stats.js
+++ utils/stats.js
@@ -1,18 +1,18 @@
+const ora = require('ora');
 const axios = require('axios');
-// const fetch = require('node-fetch')
 const handleError = require('cli-handle-error');
 const to = require('await-to-js').default;
 
+const spinner = ora({ text: '' });
 const apiURL = `https://api.github.com/users/ahmadawais`;
 
 module.exports = async () => {
-	// const res = await fetch(apiURL);
-	// const data = await res.json();
-
+	spinner.start(`Fetching GitHub followers…`);
 	const [error, res] = await to(axios.get(apiURL));
 	handleError(`API CALL FAILED`, error, true, true);
 
 	const ghFollowers = res.data.followers;
+	spinner.succeed(`Got 'em!!!`);
 
 	console.log(`
 GitHub Followers: ${ghFollowers}
